daeb7bbdb7531843975908c643fd75cafb46c7eb
Joel-Wiersma-Flatiron <Joel.Wiersma.D@gmail.com>
2020-11-20 00:16:23 -0700

    commit tree 84b183913541ea1c86e1fc2137e6d7708bfbe25f

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,1 +1,14 @@
+class Appointment
+    attr_accessor :doctor, :patient, :date
+    @@all = []
+    def initialize(date, patient, doctor)
+        self.doctor = doctor
+        self.patient = patient
+        self.date = date
+        @@all << self
+    end
 
+    def self.all
+        @@all
+    end
+end
--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,23 @@
+class Genre
+    attr_accessor :name, :song, :artist
+    @@all = []
+    def initialize(name)
+        self.name = name
+        @@all << self
+    end
 
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select {|song| song.genre == self}
+    end
+
+    def artists
+        array = []
+        songs.each {|song| array << song.artist}
+        array
+    end
+
+end
